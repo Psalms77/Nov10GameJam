e2065530abd517b85a06dc7c34ebcc507e6cccfd
Psalms77 <pc4499520@gmail.com>
2023-11-09 21:17:23 -0800

    init things

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using DG.Tweening.Core.Easing;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(GameObject.Find("Canvas"));
+        DontDestroyOnLoad(GameObject.Find("UICamera"));
+        Application.targetFrameRate = 60;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject GetPlayer()
+    {
+        return GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+
+
+
+}
