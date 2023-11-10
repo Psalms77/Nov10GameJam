65885ac3556a86a906303744169e5fdbac0671f5
Psalms77 <pc4499520@gmail.com>
2023-11-10 15:24:39 -0800

    simple planet

---
+++ Assets/Scripts/Module/Planet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public GameObject player;
+    public Rigidbody2D playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 t = - player.transform.position + this.gameObject.transform.position;
+        playerRb.AddForce(t.normalized * 5f);
+        Debug.Log("aa");
+    }
+}
--- Assets/Scripts/Module/PlayerController.cs
+++ Assets/Scripts/Module/PlayerController.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Unity.VisualScripting;
 
 public class PlayerController : Observer
 {
 
     public PlayerFSM stateMachine;
+    public GameObject planet;
 
-
-
+    public Rigidbody2D rb;
+    public float moveSpeed;
 
     private void Awake()
     {
@@ -21,12 +23,20 @@ public class PlayerController : Observer
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 t = -planet.transform.position + this.gameObject.transform.position;
         
+        if (Input.GetKey(KeyCode.D)) {
+            rb.velocity = t.Perpendicular1().normalized;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = t.Perpendicular2().normalized;
+        }
     }
 }
