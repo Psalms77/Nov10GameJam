b9c6c1af7f25de6e8acc1440abe300a9c88e053b
Psalms77 <pc4499520@gmail.com>
2023-11-11 14:19:43 -0800

    player flyback when takes damage

--- Assets/Scripts/Framework/Enums/EventEnums.cs
+++ Assets/Scripts/Framework/Enums/EventEnums.cs
@@ -3,6 +3,6 @@
 public enum EventName
 {
     SwitchGameMode,
-    PlayerTakesDmg,         // param1: float, damage value
+    PlayerTakesDmg,         // param1: float, damage value; param2: gameobject, go that deals the damage
 }
 
